70d0b9340574072c2122375fb1620d9f96db8e67
Tanishq Krishnan <kronorak@outlook.com>
2020-09-28 23:51:36 +0530

    Added new functions to array.java
    Changes included in this commit:
        - Added functions to push element, insert element at index and display elements of the static and dynamic array to the array.java program (in the dataStructures folder)
        - Comments have also been included to describe each function and statement
    
    Changes to be included in the next commit:
        - Add comments to the programs starting from cwprog7 (in codeWars folder)
        - Add comments & more functionality to all programs (in dataStructures folder)
        - Add comments to all the programs (in interviewPrep folder)
        - Complete the program list in the README.md file
        - Ensure that maxProfit program executes properly (in leetCode folder)

--- dataStructures/array.java
+++ dataStructures/array.java
@@ -40,16 +40,66 @@ class array {
 		}
 	}
 
+	//Pushes passed item to the end of the array
+	public static void push(int item, int[] arr, ArrayList<Integer> arrayList) {
+		//1st insertion for array
+		arr[0] = item;
+
+		//Don't need to know the index of the last element when inerting
+		//into a Dynamic array
+		arrayList.add(item);
+
+		displayArray(arr, arrayList);
+	}
+
+	//Inserts item at index, shifts that index's value and trailing elements
+	//to the right
+	public static void insert(int index, int item, int[] arr, ArrayList<Integer> arrayList) {
+		try {
+			//Moving all values within array 1 place to the right
+			for(int i = arr.length - 1; i > index; i--) {
+				arr[i] = arr[i-1];
+			}
+			
+			//Inserting the item at index
+			arr[index] = item;
+		}
+
+		catch(IndexOutOfBoundsException ex) {
+			System.out.println("The index is outside the range of the array");
+		}
+
+		//Moves all the elements from index to the last element to the
+		//right and inserts item at index
+		arrayList.add(index, item);
+
+		//Display contents of the Array and Array List
+		displayArray(arr, arrayList);
+	}
+
+	//Displays the elements of the Array and Array List
+	public static void displayArray(int[] arr, ArrayList<Integer> arrayList) {
+		//Displaying the contents of the Static array
+		System.out.println("Contents of the Array: ");
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+		//Displaying the contents of the Dynamic array
+		System.out.println("Contents of the Array List: ");
+		arrayList.forEach((n) -> System.out.println(n));
+	}
+
 	public static void main(String[] args) {
 		//Default value of each element in an array is equal to the
 		//default value of its type
-		int[] arr = {1,2,3,4,5};
+		int[] arr = new int[5];
 		
 		size(arr);
 
 		ArrayList<Integer> arrayList = new ArrayList<>();
 
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < 5; i++) {
 			arrayList.add(i);
 		}
 		
@@ -58,7 +108,11 @@ class array {
 		size(arrayList);
 
 		is_empty(arrayList);
-		at(10, arrayList);
 		
+		at(10, arrayList);
+
+		push(2, arr, arrayList);	
+
+		insert(0, 3, arr, arrayList);
 	}
 }
