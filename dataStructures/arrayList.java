bd2e6b7126df341a20da8f6c3d872e0dd6b8dd6b
Tanishq Krishnan <kronorak@outlook.com>
2020-10-05 23:48:41 +0530

    Updated arrayList.java in dataStructures folder
    Changes included in this commit:
       - Corrected the code in constructor
       - Included capacity function
       - at function switched to void from returning int
       - resize function now accepts parameter to set the new_capacity value
       - Functions related to insert(push, insert) now resizes the capacity to twice its size if the capacity has been reached
       - Functions related to delete(delete, remove) now resizes the capacity to half its size
       - Pop function now calls delete function to perform pop operation
       - Added comments as needed to explain the program
       - Removed unncessary lines of code from the main function
    
    Changes to be included in the next commit:
       - Add comments to the programs starting from cwprog7 (in codeWars folder)
       - Add comments & more functionality to singlyLinkedList.java (in dataStructures folder)
       - Add comments to all the programs (in interviewPrep folder)
       - Complete the program list in the README.md file
       - Ensure that maxProfit program executes properly (in leetCode folder)

--- dataStructures/arrayList.java
+++ dataStructures/arrayList.java
@@ -7,13 +7,18 @@ class arrayList {
 
 	public arrayList(int new_capacity) {
 		//Ensures that arrList will hold 5 elements
-		new_capacity = this.new_capacity;
-		arrList.ensureCapacity(new_capacity);
+		this.new_capacity = new_capacity;
+		arrList.ensureCapacity(this.new_capacity);
 	}
 
 	//Returns the number of items in the Array List
 	public int size() {
 		return arrList.size();
+	}
+
+	//Returns the current capacity of the Array List
+	public int capacity() {
+		return new_capacity;	
 	}
 
 	//Returns boolean result upon checking if arrList is empty
@@ -22,15 +27,15 @@ class arrayList {
 	}
 
 	//Returns item at given index unless index ot of bounds
-	public int at(int index) {
+	public void at(int index) {
 		try {
-			return arrList.get(index);
+			System.out.println("Element at " + index + " is " + arrList.get(index));
 		}
 
 		catch(IndexOutOfBoundsException ex) {
 			//Return Integer's min value in case the index is out of
 			//bounds
-			return Integer.MIN_VALUE;
+			System.out.println(index + " is outside the Array List's range");
 		}
 	}
 
@@ -38,7 +43,10 @@ class arrayList {
 	public void push(int item) {
 		arrList.add(item);
 
-		resize();
+		//Double the size of the Array List if the size equals capacity
+		if(size() == new_capacity) {
+			resize(new_capacity * 2);
+		}
 	}
 
 	//Inserts item at index, shifts that index's value and trailing elements
@@ -47,7 +55,10 @@ class arrayList {
 		try {
 			arrList.add(index, item);
 
-			resize();
+			//Double the size of the Array List if the size equals capacity
+			if(size() == new_capacity) {
+				resize(new_capacity * 2);
+			}
 		}
 		
 		//Catches the exception if the index isn't within Array Lists's
@@ -59,29 +70,46 @@ class arrayList {
 
 	//Prepend item to the Array List
 	public void prepend(int item) {
-		arrList.add(0, item);
+		insert(0, item);
 	}
 
 	//Remove element from the end and display the value
 	public void pop() {
-		arrList.remove(size() - 1);
-
-		resize();
+		delete(size() - 1);
 	}
 
 	//Delete item at index, shifting all trailing elements left
 	public void delete(int index) {
-		arrList.remove(index);
+		try {
+			arrList.remove(index);
 
-		resize();
+			//Halve the size of the Array List if the size equals capacity/4
+			if(size() == new_capacity / 4) {
+				resize(new_capacity / 2);
+			}
+		}
+
+		//Catch in case the Array List is empty
+		catch(IndexOutOfBoundsException ex) {
+			System.out.println("Cannot pop since Array List is empty");
+		}
 	}
 
 	//Looks for an item and removes index holding it (even if in multiple
 	//places)
 	public void remove(int item) {
+		//Remove only if Array List has content
+		if(size() == 0) {
+			System.out.println("Array List is empty");
+			return;
+		}
+
 		arrList.removeAll(Collections.singleton(item));
 
-		resize();
+		//Halve the size of the Array List if the size equals capacity/4
+		if(size() == new_capacity / 4) {
+			resize(new_capacity / 2);
+		}
 	}
 
 	//Looks for value and returns first index with that value, -1 if not
@@ -90,22 +118,10 @@ class arrayList {
 		return arrList.indexOf(item);
 	}
 
-	//Increases the capacity of the Array List to double if the size of
-	//the Array List is equal to its capacity
-	//Or when popping, if size of the Array List is 1/4th of capacity,
-	//resize to half
-	private void resize() {
-		int size = size();
-		
-		if(size == new_capacity) {
-			new_capacity *= 2;
-			arrList.ensureCapacity(new_capacity);
-		}
-
-		if(size == (new_capacity / 4)) {
-			new_capacity /= 2;
-			arrList.ensureCapacity(new_capacity);
-		}
+	//Used to change the ensured capacity of the Array List
+	private void resize(int new_capacity) {
+		this.new_capacity = new_capacity;
+		arrList.ensureCapacity(this.new_capacity);
 	}
 
 	//Display the contents of the Array List
@@ -117,14 +133,5 @@ class arrayList {
 		int capacity = 10;
 
 		arrayList al = new arrayList(capacity);
-
-		//Addings 10 values to the Array List
-		for(int i = 0; i < 10; i++) {
-			al.push(1);
-		}
-
-		al.remove(1);
-
-		al.display();
 	}
 }
