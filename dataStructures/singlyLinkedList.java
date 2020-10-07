e1d6d467eeca20f7cf009bd2239d6250033ef1d8
Tanishq Krishnan <kronorak@outlook.com>
2020-10-07 23:56:20 +0530

    Updated singlyLinkedList.java in dataStructures
    Changes included in this commit:
       - Implemented new functions such as empty,value_at and push_front
       - Included comments to increase understandability of the program
    
    Changes to be included in the next commit:
       - Add comments to the programs starting from cwprog7 (in codeWars folder)
       - Add comments & more functionality to singlyLinkedList.java (in dataStructures folder)
       - Add comments to all the programs (in interviewPrep folder)
       - Complete the program list in the README.md file
       - Ensure that maxProfit program executes properly (in leetCode folder)

--- dataStructures/singlyLinkedList.java
+++ dataStructures/singlyLinkedList.java
@@ -9,6 +9,7 @@ class node {
 }
 
 class singlyLinkedList {
+	//Holds the location of the first node
 	node head;
 
 	//Returns the number of data elements in the list
@@ -26,8 +27,54 @@ class singlyLinkedList {
 
 		return ctr;
 	}
+	
+	//Checks if the Linked List is empty or not
+	public boolean empty() {
+		if (head == null) {
+			return true;
+		}
+
+		else {
+			return false;
+		}
+	}
+
+	//Returns data value of a node at index
+	public int value_at(int index) {
+		node current = head;
+
+		//As long as the node isn't null nor is the counter index equal
+		//zero, we progress to the next node
+		while(current != null && index > 0) {
+			current = current.next;
+			index--;
+		}
+
+		//If the loop had stopped earlier due to the node being null
+		//before reaching index or index isn't above 0, we
+		//know that the index isn't within the range of the Linked List
+		if(current == null || index > 0) {
+			System.out.println("Index is out of bounds of the Linked List");
+			return Integer.MIN_VALUE;
+		}
+
+		return current.data;
+	}
+
+	//Push the node from the front of the Linked List
+	public void push_front(int value) {
+		//Create a new node
+		node n = new node(value);
+		
+		//Point the node to head
+		n.next = head;
+
+		//Make head point to the new node. Thereby moving the head
+		//towards the new 1st node
+		head = n;
+	}
 
 	public static void main(String[] args) {
-		
+		singlyLinkedList sll = new singlyLinkedList();
 	}
 }
