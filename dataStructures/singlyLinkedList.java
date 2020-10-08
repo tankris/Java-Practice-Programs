e33205efcbec53b299c1187ee0ca704614755291
Tanishq Krishnan <kronorak@outlook.com>
2020-10-08 23:57:37 +0530

    Updated singlyLinkedList in dataStructures
    Changes included in this commit:
       - value_at function logic has been changed to include the following:
         -- Checking if the Linked List
         -- Better logic to confirm if index is out of bounds
       - Implemented the following new functions:
         -- pop_front() = Removes first node and returns its data
         -- push_back(data) = Adds a new node at the end
         -- pop_back() = Removes the last node and returns its data
         -- front() = returns the data held by the 1st node
         -- back() = returns the data held by the last node
         -- insert(index, value) = Inserts new node holding value at a given index
       - Reworded some and included new comments to increase understandability of the program
    
    Changes to be included in the next commit:
       - Add comments to the programs starting from cwprog7 (in codeWars folder)
       - To singlyLinkedList.java (in dataStructures folder) add the following:
         -- Add new functions
         -- Dry run all functions from push_front(value)
         -- Test all functions
       - Add comments to all the programs (in interviewPrep folder)
       - Complete the program list in the README.md file
       - Ensure that maxProfit program executes properly (in leetCode folder)

--- dataStructures/singlyLinkedList.java
+++ dataStructures/singlyLinkedList.java
@@ -43,18 +43,25 @@ class singlyLinkedList {
 	public int value_at(int index) {
 		node current = head;
 
-		//As long as the node isn't null nor is the counter index equal
-		//zero, we progress to the next node
+		//Checks if the Linked List is empty
+		if(empty() == true) {
+			System.out.println("Linked List is empty");
+			return Integer.MIN_VALUE;
+		}
+
+		//As long as the node isn't null nor is the
+		//counter index equal zero, we progress to the next node
 		while(current != null && index > 0) {
 			current = current.next;
 			index--;
 		}
 
-		//If the loop had stopped earlier due to the node being null
-		//before reaching index or index isn't above 0, we
-		//know that the index isn't within the range of the Linked List
-		if(current == null || index > 0) {
-			System.out.println("Index is out of bounds of the Linked List");
+		//If the loop had stopped earlier due to the node 
+		//being null before reaching index or index is
+		//less than 0, we know that the index isn't within 
+		//the range of the Linked List
+		if(current == null || index < 0) {
+			System.out.println("Index is out of bounds");
 			return Integer.MIN_VALUE;
 		}
 
@@ -74,6 +81,134 @@ class singlyLinkedList {
 		head = n;
 	}
 
+	//Remove front item and return its value
+	public int pop_front() {
+		int data = head.data;
+
+		//Checks if the Linked List is empty
+		if(head == null) {
+			System.out.println("The Linked List is empty");
+			return Integer.MIN_VALUE;
+		}
+		
+		//Making head to point to the next node and so effectively
+		//removing the first element
+		head = head.next;
+
+		return data;
+	}
+
+	//Adds an item at the end
+	public void push_back(int data) {
+		node current = head;
+		node n = new node(data);
+
+		//If the Linked List is empty, then create the 1st node
+		if(head == null) {
+			head = n;
+			return;
+		}
+
+		//Find the last node
+		while(current.next != null) {
+			current = current.next;
+		}
+
+		//Make last node point to the new node
+		current.next = n;
+	}
+
+	//Removes end item and returns its value
+	public int pop_back() {
+		int data;
+		node current = head;
+
+		//Checks if the Linked List is empty
+		if(head == null) {
+			System.out.println("Linked List is empty");
+			return Integer.MIN_VALUE;
+		}
+
+		//If there is only 1 element in the Linked List,
+		//then remove it
+		if(head.next == null) {
+			head = null;
+		}
+
+		//Checks if the current node is pointing to the 
+		//last node
+		while(current.next.next != null) {
+			current = current.next;
+		}
+
+		data = current.next.data;
+		current.next = null;
+
+		return data;
+	}
+
+	//Returns the value of the 1st node in the Linked List
+	public int front() {
+		return head.data;
+	}
+
+	//Returns the value of the end node
+	public int back() {
+		node current = head;
+
+		//Checks if the Linked List is empty
+		if(current == null) {
+			System.out.println("Linked List is empty");
+			return Integer.MIN_VALUE;
+		}
+
+		//Finds the last node
+		while(current.next != null) {
+			current = current.next;
+		}
+
+		return current.data;
+	}
+
+	//Insert value at index, so current item at that 
+	//index is pointed to by new item at index
+	public void insert(int index, int value) {
+		
+		node current = head;
+		int size = size();
+
+		//Checks if index is out of bounds
+		if(index < 0 || index > size) {
+			System.out.println("Index is out of bounds");
+		}
+		
+		//Checks if value needs to be pushed in the front
+		//If so, we use the push_front function
+		if(index == 0) {
+			push_front(value);
+			return;
+		}
+
+		//Checks if value needs to be pushed at the back
+		//If so, we use the push_back function
+		if(index == size) {
+			push_back(value);
+		}
+
+		//Finds the node right before the index position
+		if(index > 1 && current.next != null) {
+			current = current.next;
+			index--;
+		}
+		
+		//Creates a new node, has it point to node at index,
+		//then has node at index-1 point to new node
+		//Hence node is not at intended index
+		node n = new node(value);
+		n.next = current.next;
+		current.next = n;
+	}
+	
 	public static void main(String[] args) {
 		singlyLinkedList sll = new singlyLinkedList();
 	}
