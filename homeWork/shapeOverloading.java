9a99276ae47db4f3f779fef4e0512e45c3889c99
Tanishq K <kronorak@outlook.com>
2021-03-07 12:26:40 +0530

    Update shapeOverloading.java
    - Updated the program to show that overloading can be performed on
    static methods as well

--- homeWork/shapeOverloading.java
+++ homeWork/shapeOverloading.java
@@ -1,13 +1,13 @@
 class shapeOverloading {
-	public void area(int side) {
+	public static void area(int side) {
 		System.out.println("Area of a square is " + (side * side));
 	}
 
-	public void area(int length, int breadth) {
+	public static void area(int length, int breadth) {
 		System.out.println("Area of a rectangle is " + (length * breadth));
 	}
 
-	public void area(float radius) {
+	public static void area(float radius) {
 		System.out.println("Area of a circle is " + (Math.PI * radius * radius));
 	}
 
